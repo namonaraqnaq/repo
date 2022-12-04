53d0bdb500c7aea124e99b357d92713ff77cae05
Nickoleta Dimitrova <namonaraqnaq@gmail.com>
2022-12-04 21:25:15 +0200

    Who commits?

---
+++ opencode_systems/http_client/http_client.cpp
@@ -0,0 +1,10 @@
+// http_client.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    std::cout << "Http Client!\n";
+}
+
---
+++ opencode_systems/http_server/http_server.cpp
@@ -0,0 +1,10 @@
+// http_server.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    std::cout << "http_server!\n";
+}
+
