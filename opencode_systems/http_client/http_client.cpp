a4be060eadc3365929e3b54e43665a481858d109
Nickoleta Dimitrova <namonaraqnaq@gmail.com>
2022-12-05 21:48:26 +0200

    Parsing command line arguments

--- opencode_systems/http_client/http_client.cpp
+++ opencode_systems/http_client/http_client.cpp
@@ -1,10 +1,57 @@
 // http_client.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
+#include <boost/regex.hpp> 
 #include <iostream>
+#include <fstream>
+#include <filesystem>
+#include <string>
+#include <map>
+
+using namespace std;
+
+static int input_params_error() {
+	cerr <<
+		"Usage: http_client --host=hostname --port=portnumber --keep-alive=seconds --post-request=seconds --reload=seconds --request=path_to_request.json" <<
+		"Example:\n" <<
+		"    http_client --host=www.example.com --port=80 --keep-alive=10 --post-request=20 --reload=30 --request=request.json";
+	return EXIT_FAILURE;
+}
 
 int main(int argc, char *argv[])
 {
-    std::cout << "Http Client!\n";
+	if (argc != 7) {
+		return input_params_error();		
+	}
+	map<string, string> settings;
+	boost::regex args_format("^--(.+)=(.+)$");
+	boost::cmatch cm;
+	for (int i = 1; i < argc; ++i) {		
+		if (!boost::regex_match(argv[i], cm, args_format)){
+			return input_params_error();
+		}
+		settings[cm[1].str()] = cm[2].str();
+	}
+
+	const auto host(settings["host"]);
+	const auto port(settings["port"]);
+	if (host.empty() || port.empty()) {
+		return input_params_error();
+	}
+
+	const auto keep_alive_secs = atoi(settings["keep-alive"].c_str());
+	const auto post_request_secs = atoi(settings["post-request"].c_str());
+	const auto reload_secs = atoi(settings["reload"].c_str());
+	if (keep_alive_secs <= 0 || post_request_secs <= 0 || reload_secs <= 0) {
+		return input_params_error();
+	}
+
+	const auto request_path(settings["request"]);
+	ifstream f(request_path.c_str());
+	if (!f.good()) {
+		cerr << "bad path_to_request" << endl;
+		return input_params_error();
+	}
+
+	return EXIT_SUCCESS;
 }
 
--- opencode_systems/http_server/http_server.cpp
+++ opencode_systems/http_server/http_server.cpp
@@ -1,10 +1,52 @@
 // http_server.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
+#include <boost/regex.hpp> 
 #include <iostream>
+#include <fstream>
+#include <filesystem>
+#include <string>
+#include <map>
+
+using namespace std;
+
+static int input_params_error() {
+	cerr <<
+		"Usage: --port=portnumber --reload=seconds --response=path_to_response.json" <<
+		"Example:\n" <<
+		"    http_server --port=80 --reload=30 --response=response.json";
+	return EXIT_FAILURE;
+}
 
 int main(int argc, char *argv[])
 {
-    std::cout << "http_server!\n";
-}
+	if (argc != 4) {
+		return input_params_error();
+	}
+	map<string, string> settings;
+	boost::regex args_format("^--(.+)=(.+)$");
+	boost::cmatch cm;
+	for (int i = 1; i < argc; ++i) {
+		if (!boost::regex_match(argv[i], cm, args_format)) {
+			return input_params_error();
+		}
+		settings[cm[1].str()] = cm[2].str();
+	}
 
+	const auto port(settings["port"]);
+	if (port.empty()) {
+		return input_params_error();
+	}
+	const auto reload_secs = atoi(settings["reload"].c_str());
+	if (reload_secs <= 0) {
+		return input_params_error();
+	}
+
+	const auto response_path(settings["response"]);
+	ifstream f(response_path.c_str());
+	if (!f.good()) {
+		cerr << "bad path_to_response" << endl;
+		return input_params_error();
+	}
+
+	return EXIT_SUCCESS;
+}
