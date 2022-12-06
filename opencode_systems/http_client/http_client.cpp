9ad1e4a3d28fda13da7800fafc9c556221d39d0f
Nickoleta Dimitrova <namonaraqnaq@gmail.com>
2022-12-06 23:51:16 +0200

    basic http server & http client functionality

--- opencode_systems/http_client/http_client.cpp
+++ opencode_systems/http_client/http_client.cpp
@@ -1,13 +1,119 @@
 // http_client.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// and implements asynchronous http client 
 //
-#include <boost/regex.hpp> 
-#include <iostream>
-#include <fstream>
+#include <boost/asio/strand.hpp>
+#include <boost/beast/core.hpp>
+#include <boost/beast/http.hpp>
+#include <boost/beast/version.hpp>
+#include <boost/regex.hpp>
+#include <cstdlib>
 #include <filesystem>
-#include <string>
+#include <fstream>
+#include <iostream>
+#include <memory>
 #include <map>
+#include <string>
 
+namespace beast = boost::beast;
+namespace http = beast::http;
+namespace net = boost::asio;
+using tcp = boost::asio::ip::tcp;
 using namespace std;
+
+static void report_failure(beast::error_code err, char const* src)
+{
+	std::cerr << src << ": " << err.message() << "\n";
+}
+
+// Performs an HTTP GET and prints the response
+class http_session 
+	: public std::enable_shared_from_this<http_session>
+{
+	tcp::resolver resolver_;
+	beast::tcp_stream stream_;
+	beast::flat_buffer buffer_; // (Must persist between reads)
+	http::request<http::empty_body> req_;
+	http::response<http::string_body> res_;
+
+public:
+	// Objects are constructed with a strand to
+	// ensure that handlers do not execute concurrently.
+	explicit http_session(net::io_context& ioc)
+		: resolver_(net::make_strand(ioc))
+		, stream_(net::make_strand(ioc))
+	{
+	}
+
+	// Start the asynchronous operation
+	void run(char const* host, char const* port, char const* target)
+	{
+		// Set up an HTTP GET request message
+		req_.method(http::verb::get);
+		req_.target(target);
+		req_.set(http::field::host, host);
+		req_.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
+
+		// Look up the domain name
+		resolver_.async_resolve(host, port,
+				beast::bind_front_handler(&http_session::on_resolve, shared_from_this()));
+	}
+
+	void on_resolve(beast::error_code err, tcp::resolver::results_type results)
+	{
+		if (err) {
+			return report_failure(err, "resolve");
+		}
+		// Set a timeout on the operation
+		stream_.expires_after(std::chrono::seconds(30));
+
+		// Make the connection on the IP address we get from a lookup
+		stream_.async_connect(results,
+				beast::bind_front_handler(&http_session::on_connect, shared_from_this()));
+	}
+
+	void on_connect(beast::error_code err, tcp::resolver::results_type::endpoint_type)
+	{
+		if (err) {
+			return report_failure(err, "connect");
+		}
+		// Set a timeout on the operation
+		stream_.expires_after(std::chrono::seconds(30));
+
+		// Send the HTTP request to the remote host
+		http::async_write(stream_, req_,
+			beast::bind_front_handler(&http_session::on_write, shared_from_this()));
+	}
+
+	void on_write(beast::error_code err, std::size_t bytes_transferred)
+	{
+		boost::ignore_unused(bytes_transferred);
+		if (err) {
+			return report_failure(err, "write");
+		}
+		// Receive the HTTP response
+		http::async_read(stream_, buffer_, res_,
+			beast::bind_front_handler(&http_session::on_read, shared_from_this()));
+	}
+
+	void on_read(beast::error_code err, std::size_t bytes_transferred)
+	{
+		boost::ignore_unused(bytes_transferred);
+		if (err) {
+			return report_failure(err, "read");
+		}
+		// Write the message to standard out
+		std::cout << res_ << std::endl;
+
+		// Gracefully close the socket
+		stream_.socket().shutdown(tcp::socket::shutdown_both, err);
+
+		// not_connected happens sometimes so don't bother reporting it.
+		if (err && err != beast::errc::not_connected)
+			return report_failure(err, "shutdown");
+
+		// If we get here then the connection is closed gracefully
+	}
+};
 
 static int input_params_error() {
 	cerr <<
